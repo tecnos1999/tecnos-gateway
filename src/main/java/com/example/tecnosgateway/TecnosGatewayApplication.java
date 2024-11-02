f0607435ff89a9ef333f94e40f8cb10fab4f9e54
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-02 19:16:57 +0200

    update

---
+++ src/main/java/com/example/tecnosgateway/TecnosGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.tecnosgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnosGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecnosGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tecnosgateway/TecnosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tecnosgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnosGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
