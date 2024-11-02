eaa29a060ff0881bc307ec0b52f21af73c862cca
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-02 19:47:25 +0200

    update

--- src/main/java/com/example/tecnosgateway/TecnosGatewayApplication.java
+++ src/main/java/com/example/tecnosgateway/TecnosGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.tecnosgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class TecnosGatewayApplication {
@@ -9,5 +12,9 @@ public class TecnosGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(TecnosGatewayApplication.class, args);
     }
-
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
 }
